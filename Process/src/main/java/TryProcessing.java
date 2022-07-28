49eea3dd544006edc09cd1303c9436121f28fd33
Pramila Ghosh <87747137+PramilaGhosh@users.noreply.github.com>
2022-07-28 16:47:11 +0530

    Process_Procedural

---
+++ Process/src/main/java/TryProcessing.java
@@ -0,0 +1,40 @@
+import processing.core.PApplet;
+
+import java.util.Dictionary;
+
+public class TryProcessing extends PApplet {
+    public static final int WIDTH = 640;
+    public static final int HEIGHT = 480;
+    public static final int DIAMETER = 10;
+    int x_pos1, x_pos2, x_pos3, x_pos4 = 0;
+
+
+    public static void main(String args[]) {
+        PApplet.main("TryProcessing", args);
+
+    }
+
+    @Override
+    public void settings() {
+        super.settings();
+        size(640, 488);
+    }
+
+
+    @Override
+    public void draw() {
+        ellipse(x_pos1, HEIGHT / 5, DIAMETER, DIAMETER);
+        x_pos1 += 1;
+
+        ellipse(x_pos2, HEIGHT / 5 * 2, DIAMETER, DIAMETER);
+        x_pos2 += 2;
+
+        ellipse(x_pos3, HEIGHT / 5 * 3, DIAMETER, DIAMETER);
+        x_pos3 += 3;
+
+        ellipse(x_pos4, HEIGHT / 5 * 4, DIAMETER, DIAMETER);
+        x_pos4 += 4;
+
+    }
+
+}
